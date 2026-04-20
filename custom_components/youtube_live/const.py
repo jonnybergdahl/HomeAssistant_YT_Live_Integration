571b9ce3ed75ab546ed102acd9511ec49e490925
Jonny Bergdahl <bergdahl@users.noreply.github.com>
2026-04-20 23:37:04 +0200

    Change Calendar poll to 10 minutes.

--- custom_components/youtube_live/const.py
+++ custom_components/youtube_live/const.py
@@ -7,7 +7,7 @@ DOMAIN = "youtube_live"
 CONF_GROUP_NAME = "group_name"
 CONF_CHANNEL_HANDLES = "channel_handles"
 
-DEFAULT_CALENDAR_INTERVAL = timedelta(hours=1)
+DEFAULT_CALENDAR_INTERVAL = timedelta(minutes=10)
 DEFAULT_SENSOR_INTERVAL = timedelta(minutes=1)
 ACTIVE_WINDOW_MINUTES = 15
 DEFAULT_STREAM_DURATION_HOURS = 2
